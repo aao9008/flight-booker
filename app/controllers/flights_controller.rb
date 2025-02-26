4b9f079e471f22a4f5073c03247c51b534e63cec
Alfredo Ormeno <ormenoandres13@gmail.com>
2025-02-26 00:26:59 -0500

    Implement flight search and selection process
    - Added a search form on the /flights index page that submits using GET.
    - Included dropdowns for departure airport, arrival airport, number of
    passengers, and available flight dates.
    - Displayed search results with radio buttons for selecting a flight.
    - Created a separate form for flight selection that submits to
    BookingsController#new.
    - Used a hidden field to preserve the number of passengers when
    selecting a flight.
    - Ensured previously selected values remain after form submission.
    - Connected flight selection to the booking process with a GET request.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,15 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
+    @available_dates = Flight.order(:start_datetime).pluck(:start_datetime).map { |dt| dt.to_date }.uniq
+    @flights = []
+
+    if params[:departure_airport].present? && params[:arrival_airport].present? && params[:start_datetime].present? && params[:passengers].present?
+      @flights = Flight.where(
+        departure_airport_id: params[:departure_airport],
+        arrival_airport_id: params[:arrival_airport],
+        start_datetime: DateTime.parse(params[:start_datetime]).all_day
+      )
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_10_000109) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_23_030136) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_10_000109) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "code"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "flights", force: :cascade do |t|
